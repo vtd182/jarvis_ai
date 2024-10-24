b096bca9289fd04b164652efc1d2caf76ba6573b
cuongtp1408 <cuongtp1408@gmail.com>
2024-10-25 00:35:56 +0700

    Improvement subscribe

--- lib/const/resource.dart
+++ lib/const/resource.dart
@@ -4,30 +4,40 @@
 class R {
   const R._();
 
-  /// ![preview](file://D:\Năm 4\mobile\jarvis_ai\assets\icon\ic_app.png)
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_app.png)
   static const String ASSETS_ICON_IC_APP_PNG = 'assets/icon/ic_app.png';
 
-  /// ![preview](file://D:\Năm 4\mobile\jarvis_ai\assets\icon\ic_gemini_1.png)
-  static const String ASSETS_ICON_IC_GEMINI_1_PNG = 'assets/icon/ic_gemini_1.png';
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_checked.png)
+  static const String ASSETS_ICON_IC_CHECKED_PNG = 'assets/icon/ic_checked.png';
 
-  /// ![preview](file://D:\Năm 4\mobile\jarvis_ai\assets\icon\ic_gemini_15.png)
-  static const String ASSETS_ICON_IC_GEMINI_15_PNG = 'assets/icon/ic_gemini_15.png';
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_gemini_1.png)
+  static const String ASSETS_ICON_IC_GEMINI_1_PNG =
+      'assets/icon/ic_gemini_1.png';
 
-  /// ![preview](file://D:\Năm 4\mobile\jarvis_ai\assets\icon\ic_gpt_35.png)
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_gemini_15.png)
+  static const String ASSETS_ICON_IC_GEMINI_15_PNG =
+      'assets/icon/ic_gemini_15.png';
+
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_gpt_35.png)
   static const String ASSETS_ICON_IC_GPT_35_PNG = 'assets/icon/ic_gpt_35.png';
 
-  /// ![preview](file://D:\Năm 4\mobile\jarvis_ai\assets\icon\ic_gpt_4.png)
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_gpt_4.png)
   static const String ASSETS_ICON_IC_GPT_4_PNG = 'assets/icon/ic_gpt_4.png';
 
-  /// ![preview](file://D:\Năm 4\mobile\jarvis_ai\assets\icon\ic_hello.png)
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_hello.png)
   static const String ASSETS_ICON_IC_HELLO_PNG = 'assets/icon/ic_hello.png';
 
-  /// ![preview](file://D:\Năm 4\mobile\jarvis_ai\assets\icon\ic_prompt.png)
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_prompt.png)
   static const String ASSETS_ICON_IC_PROMPT_PNG = 'assets/icon/ic_prompt.png';
 
-  /// ![preview](file://D:\Năm 4\mobile\jarvis_ai\assets\icon\ic_token.png)
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_token.png)
   static const String ASSETS_ICON_IC_TOKEN_PNG = 'assets/icon/ic_token.png';
 
-  /// ![preview](file://D:\Năm 4\mobile\jarvis_ai\assets\icon\ic_upload_img.png)
-  static const String ASSETS_ICON_IC_UPLOAD_IMG_PNG = 'assets/icon/ic_upload_img.png';
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_un_check.png)
+  static const String ASSETS_ICON_IC_UN_CHECK_PNG =
+      'assets/icon/ic_un_check.png';
+
+  /// ![preview](file://D:\Nam_4\mobile\jarvis_ai\assets\icon\ic_upload_img.png)
+  static const String ASSETS_ICON_IC_UPLOAD_IMG_PNG =
+      'assets/icon/ic_upload_img.png';
 }
