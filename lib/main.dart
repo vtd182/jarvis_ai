d125314efa06308a6f02cb0f245ac6efcebb4763
vtd182 <92201436+vtd182@users.noreply.github.com>
2024-11-12 21:40:03 +0700

    implement main

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:jarvis_ai/helpers/utils.dart' as utils;
-import 'package:jarvis_ai/modules/auth/app/ui/login/login_page.dart';
+import 'package:jarvis_ai/modules/home/app/ui/home_page.dart';
 
 import 'loading.dart';
 import 'locator.dart';
@@ -87,7 +87,7 @@ class _MyAppState extends State<Main> {
         home: LayoutBuilder(
           builder: (context, _) {
             WidgetsBinding.instance.addPostFrameCallback((_) => utils.insertOverlay(context, const ConnectionPage()));
-            return const LoginPage();
+            return HomePage();
           },
         ),
       ),
