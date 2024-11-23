a3b3d7cc2b6c02b5d702bb19c1ebde02f4dfaa37
vtd182 <92201436+vtd182@users.noreply.github.com>
2024-11-23 17:06:51 +0700

    remove controller

--- lib/modules/home/app/controller/appbar_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class AppBarController extends GetxController {
-  var selectedModel = "GPT-3".obs;
-
-  void updateModel(String model) {
-    selectedModel.value = model;
-  }
-}
