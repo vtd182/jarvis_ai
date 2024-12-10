0b8e4170b72623dc0a9014a1c3cc79b48f4b090c
vtd182 <92201436+vtd182@users.noreply.github.com>
2024-12-10 22:57:28 +0700

    check uc

--- lib/modules/knowledge_base/kb_auth/domain/usecase/knowledge_base_refresh_token_usecase.dart
+++ lib/modules/knowledge_base/kb_auth/domain/usecase/knowledge_base_refresh_token_usecase.dart
@@ -1,1 +1,35 @@
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:injectable/injectable.dart';
+import 'package:jarvis_ai/modules/knowledge_base/kb_auth/data/repositories/knowledge_base_auth_repository.dart';
+import 'package:jarvis_ai/storage/spref.dart';
+import 'package:suga_core/suga_core.dart';
 
+@lazySingleton
+class KnowledgeBaseRefreshTokenUseCase extends Usecase {
+  final KnowledgeBaseAuthRepository _knowledgeBaseAuthRepository;
+
+  const KnowledgeBaseRefreshTokenUseCase(this._knowledgeBaseAuthRepository);
+
+  Future<bool> run() async {
+    final refreshToken = await SPref.instance.getKBRefreshToken();
+    if (refreshToken == null) {
+      return false;
+    } else {
+      try {
+        final data = await _knowledgeBaseAuthRepository.refreshToken(refreshToken);
+        if (data is Map<String, dynamic>) {
+          final accessToken = data["token"]["accessToken"];
+
+          // Decode the JWT to get the expiration time
+          final jwt = JWT.decode(accessToken);
+          final expiresAt = jwt.payload["exp"];
+          await SPref.instance.setKBExpiresAt(expiresAt);
+          await SPref.instance.setKBAccessToken(accessToken);
+        }
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+  }
+}
