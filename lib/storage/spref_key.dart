1021e9650c9c2715396e50412e5a8421e1c30f55
vtd182 <92201436+vtd182@users.noreply.github.com>
2024-12-25 01:52:16 +0700

    fix keytoken

--- lib/storage/spref_key.dart
+++ lib/storage/spref_key.dart
@@ -2,7 +2,7 @@ class SPrefKey {
   static const keyAccessToken = 'ACCESS_TOKEN';
   static const keyRefreshToken = 'REFRESH_TOKEN';
   static const keyExpiresAt = 'EXPIRES_AT';
-  static const keyKBAccessToken = 'ACCESS_TOKEN';
-  static const keyKBRefreshToken = 'REFRESH_TOKEN';
-  static const keyKBExpiresAt = 'EXPIRES_AT';
+  static const keyKBAccessToken = 'KB_ACCESS_TOKEN';
+  static const keyKBRefreshToken = 'KB_REFRESH_TOKEN';
+  static const keyKBExpiresAt = 'KB_EXPIRES_AT';
 }
