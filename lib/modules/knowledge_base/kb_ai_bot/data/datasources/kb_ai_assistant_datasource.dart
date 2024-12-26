9adb967288da24c9a5a251408287f3bdc80ba424
vtd182 <92201436+vtd182@users.noreply.github.com>
2024-12-27 00:59:39 +0700

    implement domain and data layer

--- lib/modules/knowledge_base/kb_ai_bot/data/datasources/kb_ai_assistant_datasource.dart
+++ lib/modules/knowledge_base/kb_ai_bot/data/datasources/kb_ai_assistant_datasource.dart
@@ -1,1 +1,103 @@
+import 'package:injectable/injectable.dart';
+import 'package:jarvis_ai/modules/knowledge_base/kb_ai_bot/data/datasources/services/kb_ai_assistant_service.dart';
+import 'package:jarvis_ai/modules/knowledge_base/kb_ai_bot/domain/models/kb_ai_assistant.dart';
 
+abstract class KBAIAssistantDataSource {
+  Future<KBAIAssistant> createKBAIAssistant({
+    required String assistantName,
+    required String instructions,
+    required String description,
+  });
+
+  Future<KBAIAssistant> updateKBAIAssistant({
+    required String assistantId,
+    required String assistantName,
+    required String instructions,
+    required String description,
+  });
+
+  Future<KBAIAssistant> getKBAIAssistant({
+    required String assistantId,
+  });
+
+  Future<bool> deleteKBAIAssistant({
+    required String assistantId,
+  });
+
+  Future<List<KBAIAssistant>> getListKBAIAssistant({
+    required String query,
+    required String order,
+    required String orderField,
+    required int offset,
+    required int limit,
+    required bool isFavorite,
+    required bool isPublished,
+  });
+}
+
+@LazySingleton(as: KBAIAssistantDataSource)
+class KBAIAssistantDataSourceImp implements KBAIAssistantDataSource {
+  final KBAIAssistantService _kbAiAssistantService;
+
+  KBAIAssistantDataSourceImp(this._kbAiAssistantService);
+  @override
+  Future<KBAIAssistant> createKBAIAssistant({
+    required String assistantName,
+    required String instructions,
+    required String description,
+  }) {
+    return _kbAiAssistantService.createKBAIAssistant({
+      'assistantName': assistantName,
+      'instructions': instructions,
+      'description': description,
+    });
+  }
+
+  @override
+  Future<bool> deleteKBAIAssistant({required String assistantId}) {
+    return _kbAiAssistantService.deleteKBAIAssistant(assistantId);
+  }
+
+  @override
+  Future<KBAIAssistant> getKBAIAssistant({required String assistantId}) {
+    return _kbAiAssistantService.getKBAIAssistant(assistantId);
+  }
+
+  @override
+  Future<List<KBAIAssistant>> getListKBAIAssistant({
+    required String query,
+    required String order,
+    required String orderField,
+    required int offset,
+    required int limit,
+    required bool isFavorite,
+    required bool isPublished,
+  }) {
+    return _kbAiAssistantService.getListKBAIAssistant(
+      query,
+      order,
+      orderField,
+      offset,
+      limit,
+      isFavorite,
+      isPublished,
+    );
+  }
+
+  @override
+  Future<KBAIAssistant> updateKBAIAssistant({
+    required String assistantId,
+    required String assistantName,
+    required String instructions,
+    required String description,
+  }) {
+    return _kbAiAssistantService.updateKBAIAssistant(
+      assistantId,
+      {
+        'assistantName': assistantName,
+        'instructions': instructions,
+        'description': description,
+      },
+    );
+  }
+}
--- lib/modules/knowledge_base/kb_ai_bot/domain/usecase/delete_ai_assistant_by_id_usecase.dart
+++ lib/modules/knowledge_base/kb_ai_bot/domain/usecase/delete_ai_assistant_by_id_usecase.dart
@@ -1,1 +1,13 @@
+import 'package:injectable/injectable.dart';
+import 'package:jarvis_ai/modules/knowledge_base/kb_ai_bot/data/repositories/kb_ai_assistant_repository.dart';
 
+@lazySingleton
+class DeleteAIAssistantByIdUseCase {
+  final KBAIAssistantRepository _repository;
+
+  DeleteAIAssistantByIdUseCase(this._repository);
+
+  Future<void> run({required String assistantId}) {
+    return _repository.deleteKBAIAssistant(assistantId: assistantId);
+  }
+}
